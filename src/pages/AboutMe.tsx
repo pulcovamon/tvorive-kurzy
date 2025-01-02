0d498c02441738a6fb20104be3d2609a76e2de70
pulcovamon <pulcovamon@gmail.com>
2025-01-02 21:56:10 +0100

    adjust layout

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,23 +1,23 @@
 const AboutMe = () => {
-    return <div className="info-page">
-        <div className="text-inside-container-info">
-          <div className="lecturer">
-              <img className="avatar" src="avatar.jpg" />
-            <div className="lecturer-intro">
-              <b className="course-title">
-              <h1>O mě</h1>
-              </b>
-              <img src="line.png" height={10} width={"100%"} />
-              <i className="intro-text">
-                Ahoj, jmenuju se Eva, jsem ilustratorka knih pro deti s desetiletou praxi vyuky kresby,
-                malby a ruznych vytvarnych technik a praipravy na talentova zkousky.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </i> <br />
-              <br />
-            </div>
+  return (
+      <div className="info-page">
+          <div className="text-inside-container-info">
+              <div className="lecturer">
+                  <img className="avatar" src="avatar.jpg" alt="Profilová fotografie" />
+                  <div className="lecturer-intro">
+                      <h1 className="course-title">O mně</h1>
+                      <img src="line.png" alt="Oddělovač" height={10} width="100%" />
+                      <p className="intro-text">
+                          Ahoj, jmenuji se Eva. Jsem ilustrátorka knih pro děti s desetiletou praxí ve výuce kresby,
+                          malby, různých výtvarných technik a přípravě na talentové zkoušky.
+                          <br/><br/>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                      </p>
+                  </div>
+              </div>
           </div>
-        </div>
-    </div>
+      </div>
+  );
 };
 
 export default AboutMe;
